238f1acd51a624bb91fe332b34f295c10b56bafc
LittleElijah <cuiyeqing960904@gmail.com>
2018-12-29 22:54:40 +0300

    maring - padding

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="dashboard">
-        <h1>Team</h1>
+        <h1 class="subheading grey--text">Team</h1>
 
+        <v-container class="my-5">
+            <p>bla bla</p>
+        </v-container>
     </div>
 </template>
 

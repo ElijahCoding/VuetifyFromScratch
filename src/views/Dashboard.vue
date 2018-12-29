5ab3c6aad555c91e58aec9b7a61f277614fb4ce2
LittleElijah <cuiyeqing960904@gmail.com>
2018-12-29 23:24:20 +0300

    tooltip

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,15 +5,22 @@
         <v-container fluid class="my-5">
 
             <v-layout row justify-start class="mb-3">
-                <v-btn small flat color="grey" @click="sortBy('title')">
-                  <v-icon small left>folder</v-icon>
-                  <span class="caption text-lowercase">By project name</span>
-                </v-btn>
-                <v-btn small flat color="grey" @click="sortBy('person')">
-                  <v-icon small left>person</v-icon>
-                  <span class="caption text-lowercase">By Person</span>
-                </v-btn>
+                <v-tooltip top>
+                  <v-btn small flat color="grey" @click="sortBy('title')" slot="activator">
+                    <v-icon small left>folder</v-icon>
+                    <span class="caption text-lowercase">By project name</span>
+                  </v-btn>
+                  <span>Sort by project name</span>
+                </v-tooltip>
+                <v-tooltip top>
+                  <v-btn small flat color="grey" @click="sortBy('person')" slot="activator">
+                    <v-icon small left>person</v-icon>
+                    <span class="caption text-lowercase">By Person</span>
+                  </v-btn>
+                  <span>Sort by project author</span>
+                </v-tooltip>
             </v-layout>
+
 
             <v-card flat v-for="project in projects" :key="project.title">
                 <v-layout row wrap :class="`pa-3 project ${project.status}`">

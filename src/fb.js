b452ff44292a3c7cb0c6089adbcaf73d84929855
LittleElijah <cuiyeqing960904@gmail.com>
2018-12-30 00:13:43 +0300

    firebase configured

---
+++ src/fb.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+import 'firebase/app'
+
+var config = {
+    apiKey: "AIzaSyAnzOpVsE72rRqZfmCsFSJ6dI0XPE3IcDk",
+    authDomain: "vue-project-20372.firebaseapp.com",
+    databaseURL: "https://vue-project-20372.firebaseio.com",
+    projectId: "vue-project-20372",
+    storageBucket: "vue-project-20372.appspot.com",
+    messagingSenderId: "1019969519515"
+  }
+ firebase.initializeApp(config)
+
+ const db = firebase.firestore()
+ db.settings({ timestampsInSnapshots: true })
+
+ export default db
